0afec8dadbdc180b6097bba5badc3a5047f23d7d
verenx <verenx@gmail.com>
2024-03-05 09:49:53 +0100

    updated prompt

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ let searchedWordElement = document.querySelector("#search-field");
 console.log(`Generating word definition of ${searchedWordElement.value}`)
 
 let apiKey = "bbb7o8acc1a3c8e9f87003f9494b2cct";
-let prompt = `Please provide a precise word definition for ${searchedWordElement.value}. Produce the definition in basic HTML format and give it a heading of <h3> Definition </h3>. After this add only one example sentence with this word ${searchedWordElement.value}. Produce the example sentence in basic HTML format and give it a heading of <h3> Example </h3>. Put your signature as a line at the end like this: <em> SheCodes AI </em>`;
+let prompt = `Please provide a precise word definition for ${searchedWordElement.value}. Produce the definition in basic HTML format and give it a heading of <h3> Definition </h3>. After this add only one example sentence. The sentence must include this word ${searchedWordElement.value} and show how this word can be used in a sentence. Produce the example sentence in basic HTML format and give it a heading of <h3> Example </h3>. Put your signature as a line at the end like this: <em> SheCodes AI </em>`;
 let context = "You are a very intelligent AI assistant and you know all the word definitions in this world. You can define words in a very short and precise way. Please only include the definition of the word and the examples in your answer, don't make any polite introductory remarks.";
 
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;

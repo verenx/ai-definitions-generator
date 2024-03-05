613d63af29443ec3ef938e3b04a7f3482a2888e5
verenx <verenx@gmail.com>
2024-03-05 11:14:27 +0100

    improved UX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayDefinition(response) {
-    console.log(response.data);
+  
 
     new Typewriter('#generated-definition', {
         strings: response.data.answer,
@@ -13,8 +13,9 @@ function generateDefinition(event) {
 event.preventDefault();
 
 let searchedWordElement = document.querySelector("#search-field");
-
-console.log(`Generating word definition of ${searchedWordElement.value}`)
+let definitionBox = document.querySelector("#generated-definition");
+definitionBox.classList.remove("hidden");
+definitionBox.innerHTML = `<div class="generating-blink">⏳Generating a definition for <strong>${searchedWordElement.value}</strong></div>`;
 
 let apiKey = "bbb7o8acc1a3c8e9f87003f9494b2cct";
 let prompt = `Please provide a precise word definition for ${searchedWordElement.value}. Produce the definition in basic HTML format and give it a heading of <h3> Definition </h3>. After this add only one example sentence. The sentence must include this word ${searchedWordElement.value} and show how this word can be used in a sentence. Produce the example sentence in basic HTML format and give it a heading of <h3> Example </h3>. Put your signature as a line at the end like this: <em> SheCodes AI </em>`;
